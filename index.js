e56a158a6bc0bd8ee48494865a2f24c036c485ce
Ben <892156860@qq.com>
2018-04-02 22:14:55 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,24 @@
+const loaderUtils = require('loader-utils')
+const renderWxml = require('./render/render-wxml')
+const renderWxss = require('./render/render-wxss')
+const renderScript = require('./render/render-script')
+const { parseComponent } = require('vue-template-compiler')
+
+module.exports = function (content) {
+    this.cacheable()
+    var callback = this.async()
+
+    const parts = parseComponent(content)
+    if (parts.template) {
+        renderWxml.call(this, parts.template)
+    }
+    if (parts.styles && parts.styles.length) {
+        renderWxss.call(this, parts.styles[0])
+    }
+    if (parts.script) {
+        renderScript.call(this, parts.script, cb)
+    } else {
+        callback(null, '')
+    }
+}
+
---
+++ render/render-script.js
@@ -0,0 +1,27 @@
+const con = require('consolidate')
+const loaderUtils = require('loader-utils')
+const fs = require('fs-extra')
+const { resolve } = require('path')
+const { transform } = require('babel-core')
+
+module.exports = function (script, callback) {
+    this.cacheable()
+
+    const options = loaderUtils.getOptions(this)
+    const fullPath = loaderUtils.interpolateName(this, `[path][name].mina`, options)
+    const filename = loaderUtils.interpolateName(this, `[name].js`, options)
+    const folder = loaderUtils.interpolateName(this, `[folder]`, options)
+    const filePath = loaderUtils.interpolateName(this, `[path]`, options)
+
+    let result = transform(script.content, {
+        presets: [
+            [
+                'env', {
+                    modules: false
+                }
+            ]
+        ]
+    })
+
+    callback(null, result.code)
+}
